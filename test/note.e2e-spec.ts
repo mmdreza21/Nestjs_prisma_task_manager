fab8781675f48362936ce0959ab7c2db3063e1d2
mmdreza21 <mrezaj11@gmail.com>
2025-10-19 09:16:15 +0330

    test: add end-to-end tests for note creation functionality

--- test/note.e2e-spec.ts
+++ test/note.e2e-spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { NoteModule } from 'src/note/note.module';
+import { ObjectId } from 'bson';
+import { NoteService } from 'src/note/note.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  const mockRepository = {
+    // Simple sugar function for: jest.fn().mockImplementation(() => Promise.resolve(value));
+    create: jest.fn().mockResolvedValue({
+      id: new ObjectId(),
+      title: 'Test Note',
+      content: 'Hello world',
+    }),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [NoteModule],
+    })
+      .overrideProvider(NoteService)
+      .useValue(mockRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
